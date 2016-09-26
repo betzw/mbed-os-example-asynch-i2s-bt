73553cf576e23d7f5dd1f953ecc1541217b581e4
Wolfgang Betz <wolfgang.betz@st.com>
2016-09-26 14:21:59 +0200

    Change inital comment

--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2015 ARM Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/*
+  Mbed OS 5 example for asynchronous I2S/DMA streaming between BT front end & 
+  backend based on STa350BW
  */
 #include "mbed.h"
 #include "I2S.h"
